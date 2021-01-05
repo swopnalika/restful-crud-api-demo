d4d0e3b4626c5efe69a087384d5b8f24fc313117
swopnalika mohapatra <swopnalikamohapatra185@gmail.com>
2021-01-05 13:04:25 +0530

    entity added

--- src/main/java/com/example/restfulcrudapidemo/RestfulCrudApiDemoApplication.java
+++ src/main/java/com/example/restfulcrudapidemo/RestfulCrudApiDemoApplication.java
@@ -2,8 +2,12 @@ package com.example.restfulcrudapidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
 public class RestfulCrudApiDemoApplication {
 
 	public static void main(String[] args) {
